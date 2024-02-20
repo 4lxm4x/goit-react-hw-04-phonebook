5947fd54c6447329d9f25b95756d5b4e259461a5
Oleksandr Maksymenko <machinery69@gmail.com>
2024-02-20 18:27:18 +0200

    minor fix

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -2,11 +2,7 @@ import './ContactForm.css';
 import { Component } from 'react';
 import { nanoid } from 'nanoid';
 class ContactForm extends Component {
-  //     constructor(props) {
-  //         super();
-  //         // props.onFormSubmit();
 
-  // }F
 
   state = {
     id: '',
@@ -42,7 +38,6 @@ class ContactForm extends Component {
           className="contactInput"
           placeholder="Plase input your name"
           name="name"
-          // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
           onChange={this.onHandleInput}
@@ -55,7 +50,6 @@ class ContactForm extends Component {
           className="contactInput"
           placeholder="Please input your phone number"
           name="number"
-          // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
           onChange={this.onHandleInput}
